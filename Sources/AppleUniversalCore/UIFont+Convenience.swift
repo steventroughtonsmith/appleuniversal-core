39e50bfc3b45e6815a70d11b829c0206094e7bf6
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2023-07-08 16:39:03 +0100

    Adds simple UIFont convenience functions

---
+++ Sources/AppleUniversalCore/UIFont+Convenience.swift
@@ -0,0 +1,36 @@
+//
+//  UIFont+Convenience.swift
+//  MobileRadio
+//
+//  Created by Steven Troughton-Smith on 08/07/2022.
+//  Copyright © 2022 High Caffeine Content. All rights reserved.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+@available(iOS 13.0, *)
+extension UIFont {
+	
+	func rounded() -> UIFont {
+		
+		if let descriptor = self.fontDescriptor.withDesign(.rounded) {
+			return UIFont(descriptor: descriptor, size: self.pointSize)
+		}
+		else {
+			return self
+		}
+	}
+	
+	func bold() -> UIFont {
+		
+		if let descriptor = self.fontDescriptor.withSymbolicTraits(.traitBold) {
+			return UIFont(descriptor: descriptor, size: self.pointSize)
+		}
+		else {
+			return self
+		}
+	}
+	
+}
+#endif
